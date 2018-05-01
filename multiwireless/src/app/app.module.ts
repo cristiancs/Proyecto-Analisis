72895266ab8dbcd5eedc1a6ccf8c8a165ec24bd7
Cristian Navarrete <cristiannavarrete@outlook.com>
2018-05-01 14:36:35 -0300

    material Design.

--- multiwireless/src/app/app.module.ts
+++ multiwireless/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'hammerjs';
 
 
 import { AppComponent } from './app.component';
-
+import { materialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    materialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
